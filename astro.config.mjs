6119a991b0dd9254f40ae38cf17407b3bcb1bd5e
Evgeny <doronine222@gmail.com>
2026-06-07 19:29:35 +0700

    Добавление:   - шаблона "Layout"   - компонента "Button"   - компонента "ButtonMenu"   - компонента "Logo"   - шрифтов   - стилей   - favicon
    Создание:
      - секции "Header"
    
    Изменение:
      - файла конфигурации "astro.config.mjs"
      - файла конфигурации "tsconfig.json"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: '@/',
+          replacement: path.resolve('src') + '/',
+        },
+      ],
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "${ path.resolve(__dirname, 'src/styles/helpers').replace(/\\/g, '/') }" as *;`,
+        },
+      },
+    },
+  },
+})
