708ff412f3ff3d1c95ab3669ee483fb3a469057d
Evgeny <doronine222@gmail.com>
2026-06-21 22:49:57 +0700

    Изменение конфига Astro
      - добавил тему andromeeda для подсветки кода

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-import icon from 'astro-icon'
+import { defineConfig } from "astro/config"
+import path from "path"
+import { fileURLToPath } from "url"
+import icon from "astro-icon"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Zheny179.github.io',
-  base: '/PyStart/',
-  trailingSlash: 'always',
+  site: "https://Zheny179.github.io",
+  base: "/PyStart/",
+  trailingSlash: "always",
+
+  markdown: {
+    shikiConfig: {
+      theme: "andromeeda",
+    },
+  },
 
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'),
-      }
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@use "${ path.resolve(__dirname, './src/styles/helpers').replace(/\\/g, '/') }" as *;`,
+          additionalData: `@use "${path.resolve(__dirname, "./src/styles/helpers").replace(/\\/g, "/")}" as *;`,
         },
       },
     },
@@ -31,7 +36,7 @@ export default defineConfig({
 
   integrations: [
     icon({
-      iconDir: 'src/assets/icons',
-    })
+      iconDir: "src/assets/icons",
+    }),
   ],
 })
